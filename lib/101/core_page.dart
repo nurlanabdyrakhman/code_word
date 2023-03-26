cda416cd53a226c72018911432cfce38cf763831
Nurlan <abdyraknurlan@gmail.com>
2023-03-26 21:35:45 +0300

    25.03

--- lib/101/core_page.dart
+++ lib/101/core_page.dart
@@ -11,10 +11,40 @@ class _CorePageState extends State<CorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.mail,color: Colors.green,)),
+          Text('Iconu bas')
+        ],
+      ),
       body: Center(
-        child: Text(
-          'bbb',
-          style: TextStyle(color: Colors.white, fontSize: 33),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 55),
+              child: SizedBox(
+                width: 200,
+                height: 100,
+                child: Card(
+                  
+                shape: StadiumBorder(),
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      'bbb',
+                      style: TextStyle(color: Colors.white, fontSize: 33),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
