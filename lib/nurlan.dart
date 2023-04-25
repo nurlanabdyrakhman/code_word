dffc11847a0b656b66323c0454ccb16a48dee7f8
Nurlan <abdyraknurlan@gmail.com>
2023-04-26 00:19:53 +0300

    DDD

--- lib/nurlan.dart
+++ lib/nurlan.dart
@@ -8,6 +8,9 @@ class Nurlan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Salam'),
+      ),
       body: Container(
         child: ListView.builder(
           itemCount: products.length,
